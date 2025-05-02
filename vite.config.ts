d491fbd5a39a5f76928e3452e6832eabce9755df
paul <paul0802@icloud.com>
2025-05-02 22:01:39 +0200

    removed outdated proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Removes "/api" prefix
-      },
-    },
-    strictPort: true,
-    port: 4173
+    host: true
   },
 })
