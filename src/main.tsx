8179c76546a51aee55aa78a2633875f071bb947e
paul <paul0802@icloud.com>
2025-05-04 13:34:49 +0200

    changed redirect uri for auth0

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
             domain="dev-wytn16b0qo3jiulg.us.auth0.com"
             clientId="6mfsMp7uCCcpGVdRgjk6nhjcSQyNbnf6"
             authorizationParams={{
-                redirect_uri: window.location + "/creator",
+                redirect_uri: window.location + "",
             }}
             cacheLocation="localstorage"
             useRefreshTokens={true}
